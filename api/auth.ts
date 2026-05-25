212ad2c24f6201f390bf8b547d6a915e9c1e7c78
A_Dawg <abedinawab1@gmail.com>
2026-05-24 21:51:41 -0400

    fix: rename api files to typescript

--- api/auth.ts
+++ api/auth.ts
@@ -1,23 +1,19 @@
 import { supabase } from '../supabaseClient'
 
-// Admin login
-export async function login(email, password) {
+export async function login(email: string, password: string) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password
   })
-
   if (error) throw error
   return data
 }
 
-// Admin logout
 export async function logout() {
   const { error } = await supabase.auth.signOut()
   if (error) throw error
 }
 
-// Check if admin is logged in
 export async function getSession() {
   const { data } = await supabase.auth.getSession()
   return data.session
